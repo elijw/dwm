aacb895002f5fd71ac56dc2d3150a38cd2a38163
elijw <git@elijw.com>
2025-09-10 23:31:32 -0700

    dwmc stuff

--- config.def.h
+++ config.def.h
@@ -130,17 +130,71 @@ static const Key keys[] = {
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-};
+/*
+ *    dwmc signal wrappers
+ */
+void setlayoutex(const Arg* arg)
+{
+    setlayout(&((Arg){.v = &layouts[arg->i]}));
+}
 
+void viewex(const Arg* arg)
+{
+    view(&((Arg){.ui = 1 << arg->ui}));
+}
+
+void viewall(const Arg* arg)
+{
+    view(&((Arg){.ui = ~0}));
+}
+
+void toggleviewex(const Arg* arg)
+{
+    toggleview(&((Arg){.ui = 1 << arg->ui}));
+}
+
+void tagex(const Arg* arg)
+{
+    tag(&((Arg){.ui = 1 << arg->ui}));
+}
+
+void toggletagex(const Arg* arg)
+{
+    toggletag(&((Arg){.ui = 1 << arg->ui}));
+}
+
+void tagall(const Arg* arg)
+{
+    tag(&((Arg){.ui = ~0}));
+}
+
+/* signal definitions */
+/* signum must be greater than 0 */
+/* trigger signals using `xsetroot -name "fsignal:<signame> [<type> <value>]"`
+ */
+static Signal signals[] = {
+    /* signum           function */
+    {"focusstack",     focusstack    },
+    {"setmfact",       setmfact      },
+    {"togglebar",      togglebar     },
+    {"incnmaster",     incnmaster    },
+    {"togglefloating", togglefloating},
+    {"focusmon",       focusmon      },
+    {"tagmon",         tagmon        },
+    {"zoom",           zoom          },
+    {"view",           view          },
+    {"viewall",        viewall       },
+    {"viewex",         viewex        },
+    {"toggleview",     view          },
+    {"toggleviewex",   toggleviewex  },
+    {"tag",            tag           },
+    {"tagall",         tagall        },
+    {"tagex",          tagex         },
+    {"toggletag",      tag           },
+    {"toggletagex",    toggletagex   },
+    {"killclient",     killclient    },
+    {"quit",           quit          },
+    {"setlayout",      setlayout     },
+    {"setlayoutex",    setlayoutex   },
+    {"xrdb",           xrdb          },
+};
