c9e9fc636a5141a8ac7b902321d52876090547aa
elijw <git@elijw.com>
2025-09-10 23:37:37 -0700

    button definitions

--- config.def.h
+++ config.def.h
@@ -189,8 +189,23 @@ static const Key keys[] = {
 };
 
 /* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
+ * ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
+    /* click                event mask      button          function argument */
+    {ClkLtSymbol,   0,     Button1, setlayout,      {0}               },
+    {ClkLtSymbol,   0,     Button3, setlayout,      {.v = &layouts[2]}},
+    {ClkWinTitle,   0,     Button2, zoom,           {0}               },
+    {ClkStatusText, 0,     Button2, spawn,          {.v = termcmd}    },
+    {ClkClientWin,  SUPER, Button1, movemouse,      {0}               },
+    {ClkClientWin,  SUPER, Button2, togglefloating, {0}               },
+    {ClkClientWin,  SUPER, Button3, resizemouse,    {0}               },
+    {ClkTagBar,     0,     Button1, view,           {0}               },
+    {ClkTagBar,     0,     Button3, toggleview,     {0}               },
+    {ClkTagBar,     SUPER, Button1, tag,            {0}               },
+    {ClkTagBar,     SUPER, Button3, toggletag,      {0}               },
+};
+
 /*
  *    dwmc signal wrappers
  */
