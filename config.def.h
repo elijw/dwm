43fa9a1b98aae8fa8e89b1171dd1f11fbafce01e
elijw <git@elijw.com>
2025-09-10 23:29:13 -0700

    movestack patch

--- config.def.h
+++ config.def.h
@@ -88,23 +88,12 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+#include "movestack.c"
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+    {SUPER | ShiftMask,               XK_j,      movestack,      {.i = +1}                       },
+    {SUPER | ShiftMask,               XK_k,      movestack,      {.i = -1}                       },
 	//{SUPER | Mod1Mask,                XK_u,      incrgaps,       {.i = +1} },
     //{SUPER | Mod1Mask | ShiftMask,    XK_u,      incrgaps,       {.i = -1} },
     //{SUPER | Mod1Mask,                XK_i,      incrigaps,      {.i = +1} },
