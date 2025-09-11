2519dee8ddb44a85d018a5381ef9fb094bf378cf
elijw <git@elijw.com>
2025-09-10 23:33:44 -0700

    pywal colors + other styling

--- config.def.h
+++ config.def.h
@@ -37,17 +37,29 @@ static const int refreshrate = 60;
 static const int refreshrate = 144;
 #endif
 
-static const char* fonts[] = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char* fonts[] = {"JetBrainsMono Nerd Font", "monospace:size=12"};
+static const char dmenufont[] = "monospace:size=10";
+static char normbgcolor[] = "#222222";
+static char normbordercolor[] = "#444444";
+static char normfgcolor[] = "#bbbbbb";
+static char selfgcolor[] = "#eeeeee";
+static char selbordercolor[] = "#005577";
+static char selbgcolor[] = "#005577";
+
+// PATCH_alpha
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+
+static const unsigned int alphas[][3] = {
+    /*               fg      bg        border*/
+    [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
+    [SchemeSel] = {OPAQUE, baralpha, borderalpha},
+};
+
+static const char* colors[][3] = {
+    /*               fg           bg           border   */
+    [SchemeNorm] = {normfgcolor, normbgcolor, normbordercolor},
+    [SchemeSel] = {selfgcolor,  selbgcolor,  selbordercolor },
 };
 
 /* tagging */
